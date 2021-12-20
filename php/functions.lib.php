a27b14efed75cf15ef67ca06da1b09a4a779f7ec
Hacene Sadoudi <hacene.sadoudi@alumni.univ-avignon.fr>
2021-12-20 17:29:13 +0100

    feat(db): Improve connection error handling
    Show the error message and the code of the error

--- php/functions.lib.php
+++ php/functions.lib.php
@@ -10,11 +10,14 @@ function connecter() {
     $con = mysqli_connect(BD_HOST,BD_USER,BD_PASS,BD_NAME);
 
     if($con !== false) {
-        mysqli_set_charset($con, 'utf8');
+        mysqli_set_charset($con, 'utf8') or exit("Error while setting charset utf8");
         echo "Connected Successfully";
         return $con;
-    }else {
-        echo mysqli_connect_error();
     }
+    
+    $msg = 'Database connexion failed: ';
+    $msg .= mysqli_connect_error();
+    $msg .= ' : ' . mysqli_connect_errno();
+    exit($msg);
     
 }
